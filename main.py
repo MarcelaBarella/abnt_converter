88e2000e83ce77675ac596e0de3cfbe79a650c55
Marcela Barella <marcela.barella@hotmail.com>
2020-04-29 09:04:15 -0300

    add cli commands to run the converter

---
+++ main.py
@@ -0,0 +1,11 @@
+import click
+from src.abnt_converter import convert_to_abnt
+
+@click.command()
+@click.option('--name',prompt='Write the name to be converted', help='Name to be converted to ABNT')
+
+def main(name):
+    print(convert_to_abnt(name))
+
+if __name__ == '__main__':
+    main()
